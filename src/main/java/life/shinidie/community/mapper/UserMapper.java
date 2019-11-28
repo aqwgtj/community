2f6c1a151ef7e7cad4eb3475baa42cc9c89a7cac
aqwgtj <2281403837@qq.com>
2019-11-28 23:25:41 +0800

    add check cookies

--- src/main/java/life/shinidie/community/mapper/UserMapper.java
+++ src/main/java/life/shinidie/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.shinidie.community.mapper;
 import life.shinidie.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +14,7 @@ public interface UserMapper {
 //    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreat},#{gmtModified}")
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreat},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
